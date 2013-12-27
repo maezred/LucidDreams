98e376413713440f44bd572af4af25d99c5c2b8d
Matthew Oltendorf <moltendorf@gmail.com>
2013-12-27 00:52:44 -0800

    Removed old import.

--- src/com/moltendorf/bukkit/luciddreams/Listeners.java
+++ src/com/moltendorf/bukkit/luciddreams/Listeners.java
@@ -1,7 +1,6 @@
 package com.moltendorf.bukkit.luciddreams;
 
 import java.util.Arrays;
-//import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
